b7ee78a70931fe94c32857952f738fcecd22070c
Ishaan <ishaangupta240@gmail.com>
2024-09-18 19:49:14 +0530

    test 2

---
+++ .nuxt/dist/server/_nuxt/entry-styles.URl_9LWA.mjs
@@ -0,0 +1,2 @@
+import style_0 from './entry-styles-1.mjs-Bf3o1wln.js';
+export default [style_0]
---
+++ .nuxt/dist/server/styles.mjs
@@ -0,0 +1,10 @@
+const interopDefault = r => r.default || r || []
+export default {
+  "node_modules/nuxt/dist/app/entry.js": () => import('./_nuxt/entry-styles.URl_9LWA.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/welcome.vue": () => import('./_nuxt/welcome-styles.CyAsta2g.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/welcome.vue?vue&type=style&index=0&scoped=4521240c&lang.css": () => import('./_nuxt/entry-styles.URl_9LWA.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue": () => import('./_nuxt/error-404-styles.BcoFjqgl.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue": () => import('./_nuxt/error-500-styles.DPP87-Dp.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-500.vue?vue&type=style&index=0&scoped=f7ad9679&lang.css": () => import('./_nuxt/error-500-styles.DqmriZ_b.mjs').then(interopDefault),
+  "node_modules/nuxt/dist/app/components/error-404.vue?vue&type=style&index=0&scoped=00b6b518&lang.css": () => import('./_nuxt/error-404-styles.DRLC0U6S.mjs').then(interopDefault)
+}
